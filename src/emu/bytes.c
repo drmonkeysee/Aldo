b8d5b5a4ed05dcf5fa7ae5f64e82b3a6768eda0d
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-02-06 23:20:29 -0800

    shorten utility names

--- src/emu/bytes.c
+++ src/emu/bytes.c
@@ -7,8 +7,7 @@
 
 #include "bytes.h"
 
-extern inline uint16_t bytes_to_word(uint8_t, uint8_t);
-extern inline uint16_t bytea_to_word(const uint8_t[static 2]);
-extern inline void word_to_bytes(uint16_t, uint8_t *restrict,
-                                 uint8_t *restrict);
-extern inline void word_to_bytea(uint16_t, uint8_t[static 2]);
+extern inline uint16_t byt_tow(uint8_t, uint8_t);
+extern inline uint16_t bya_tow(const uint8_t[static 2]);
+extern inline void byt_frw(uint16_t, uint8_t *restrict, uint8_t *restrict);
+extern inline void bya_frw(uint16_t, uint8_t[static 2]);
--- src/emu/bytes.h
+++ src/emu/bytes.h
@@ -13,26 +13,25 @@
 // NOTE: undefined behavior if any pointer arguments are null or have
 // too few elements.
 
-inline uint16_t bytes_to_word(uint8_t lo, uint8_t hi)
+inline uint16_t byt_tow(uint8_t lo, uint8_t hi)
 {
     return lo | (hi << 8);
 }
 
-inline uint16_t bytea_to_word(const uint8_t bytes[static 2])
+inline uint16_t bya_tow(const uint8_t bytes[static 2])
 {
-    return bytes_to_word(bytes[0], bytes[1]);
+    return byt_tow(bytes[0], bytes[1]);
 }
 
-inline void word_to_bytes(uint16_t word, uint8_t *restrict lo,
-                          uint8_t *restrict hi)
+inline void byt_frw(uint16_t word, uint8_t *restrict lo, uint8_t *restrict hi)
 {
     *lo = word & 0xff;
     *hi = word >> 8;
 }
 
-inline void word_to_bytea(uint16_t word, uint8_t bytes[static 2])
+inline void bya_frw(uint16_t word, uint8_t bytes[static 2])
 {
-    word_to_bytes(word, bytes, bytes + 1);
+    byt_frw(word, bytes, bytes + 1);
 }
 
 #endif

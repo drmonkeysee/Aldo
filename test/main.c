507f296aa6ed591645822cd520b6acd28baaed6d
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-02-24 19:04:20 -0800

    add test folder

---
+++ test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Aldo-Tests
+//
+//  Created by Brandon Stansbury on 2/24/21.
+//
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[argc+1])
+{
+    (void)argv;
+    return EXIT_SUCCESS;
+}

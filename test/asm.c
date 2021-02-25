761bc216531822175de0d34c81a600d85610ddf9
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-02-24 22:06:01 -0800

    replace fake test with real tests

--- test/asm.c
+++ test/asm.c
@@ -8,16 +8,23 @@
 #include "asm.h"
 #include "ciny.h"
 
-static void test(void *context)
+static void dis_errstr_returns_known_err(void *context)
+{
+    const char *const err = dis_errstr(ASM_FMT_FAIL);
+    ct_assertequalstr("OUTPUT FAIL", err);
+}
+
+static void dis_errstr_returns_unknown_err(void *context)
 {
     const char *const err = dis_errstr(10);
-    ct_assertfail("Fake Test: %s", err);
+    ct_assertequalstr("UNKNOWN ERR", err);
 }
 
 struct ct_testsuite asm_tests(void)
 {
     static const struct ct_testcase tests[] = {
-        ct_maketest(test),
+        ct_maketest(dis_errstr_returns_known_err),
+        ct_maketest(dis_errstr_returns_unknown_err),
     };
 
     return ct_makesuite(tests);

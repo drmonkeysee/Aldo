9ed98e8e761dc34575b156964228b2d36555c747
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-03-01 19:19:11 -0800

    remove fake unk abs instruction

--- src/emu/decode.c
+++ src/emu/decode.c
@@ -13,8 +13,7 @@
 // Decoding table for all official MOS6502
 // and unofficial Ricoh 2A03 opcodes.
 const struct decoded Decode[] = {
-    // TODO: pretend 00 opcode takes 3 bytes to test UI layout
-    {IN_UNK, AM_ABS},    // 00 - BRK
+    UNK,    // 00 - BRK
     UNK,    // 01 - ORA (zp,X)
     UNK,    // 02 - Unofficial
     UNK,    // 03 - Unofficial

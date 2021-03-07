ff1f3c2dbc13c4518735830b9fec2d373e99056c
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-03-07 14:11:03 -0800

    pull test helpers into separate file

---
+++ test/emu/cpuhelp.h
@@ -0,0 +1,20 @@
+//
+//  cpuhelp.h
+//  Aldo-Tests
+//
+//  Created by Brandon Stansbury on 3/7/21.
+//
+
+#ifndef AldoTests_emu_cpuhelp_h
+#define AldoTests_emu_cpuhelp_h
+
+#include <stdint.h>
+
+#include "emu/cpu.h"
+
+extern const uint8_t bigrom[];
+
+void setup_cpu(struct mos6502 *cpu);
+int clock_cpu(struct mos6502 *cpu);
+
+#endif

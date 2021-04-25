ff047c609716d35c82d9320288ddbcbca5b2c7cc
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-04-24 21:50:42 -0700

    test irq and nmi timing

--- src/emu/nes.c
+++ src/emu/nes.c
@@ -35,6 +35,7 @@ static void load_prg(nes *self, size_t sz, const uint8_t prg[restrict sz])
         self->cart[(i + CpuRomMinAddr) & CpuRomAddrMask] = prg[i];
     }
     wrtoba(CpuRomMinAddr, self->cart + (ResetVector & CpuRomAddrMask));
+    wrtoba(0x002b, self->cart + (NmiVector & CpuRomAddrMask));
 
     // TODO: throw random stuff into RAM for testing
     for (size_t i = 0; i < RAM_SIZE; ++i) {

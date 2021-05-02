2e899c1f091ccdb4214ee21dbed9c57b76693ef3
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-01 18:10:55 -0700

    stub out cart

---
+++ src/emu/cart.c
@@ -0,0 +1,8 @@
+//
+//  cart.c
+//  Dev
+//
+//  Created by Brandon Stansbury on 5/1/21.
+//
+
+#include "cart.h"
---
+++ src/emu/cart.h
@@ -0,0 +1,13 @@
+//
+//  cart.h
+//  Dev
+//
+//  Created by Brandon Stansbury on 5/1/21.
+//
+
+#ifndef cart_h
+#define cart_h
+
+#include <stdio.h>
+
+#endif /* cart_h */

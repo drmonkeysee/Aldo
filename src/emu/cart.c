c5eb946df7def4e6c3f2596594e0a5a13f7b9ea8
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-01 18:54:42 -0700

    implement cart loading

--- src/emu/cart.c
+++ src/emu/cart.c
@@ -1,8 +1,61 @@
 //
 //  cart.c
-//  Dev
+//  Aldo
 //
 //  Created by Brandon Stansbury on 5/1/21.
 //
 
 #include "cart.h"
+
+#include "traits.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// A NES game cartridge including PRG and CHR ROM banks,
+// WRAM or save-state RAM, memory mapper circuitry, and metadata headers.
+
+struct cartridge {
+    uint8_t prg[ROM_SIZE];  // TODO: assume a single bank of 32KB PRG for now
+};
+
+//
+// Public Interface
+//
+
+const char *cart_errstr(int error)
+{
+    switch (error) {
+#define X(s, v, e) case s: return e;
+        CART_ERRCODE_X
+#undef X
+    default:
+        return "UNKNOWN ERR";
+    }
+}
+
+int cart_create(cart *c, FILE *f)
+{
+    assert(c != NULL);
+    assert(f != NULL);
+
+    struct cartridge *const self = malloc(sizeof *self);
+    const size_t bufsize = sizeof self->prg / sizeof self->prg[0],
+                 count = fread(self->prg, sizeof self->prg[0], bufsize, f);
+    if (feof(f)) {
+        c = self;
+        return 0;
+    } else if (ferror(f)) {
+        return CART_IO_ERR;
+    } else if (count == bufsize) {
+        return CART_PRG_SIZE;
+    }
+    return CART_UNKNOWN;
+}
+
+void cart_free(cart *self)
+{
+    free(self);
+}
--- src/emu/cart.h
+++ src/emu/cart.h
@@ -1,13 +1,35 @@
 //
 //  cart.h
-//  Dev
+//  Aldo
 //
 //  Created by Brandon Stansbury on 5/1/21.
 //
 
-#ifndef cart_h
-#define cart_h
+#ifndef Aldo_emu_cart_h
+#define Aldo_emu_cart_h
 
 #include <stdio.h>
 
-#endif /* cart_h */
+// X(symbol, value, error string)
+#define CART_ERRCODE_X \
+X(CART_IO_ERR, -1, "FILE READ ERROR") \
+X(CART_PRG_SIZE, -2, "PROGRAM TOO LARGE") \
+X(CART_UNKNOWN, -3, "UNKNOWN CART LOAD ERROR")
+
+enum {
+#define X(s, v, e) s = v,
+    CART_ERRCODE_X
+#undef X
+};
+
+typedef struct cartridge cart;
+
+// NOTE: returns a pointer to a statically allocated string;
+// **WARNING**: do not write through or free this pointer!
+const char *cart_errstr(int error);
+
+// NOTE: if cart_create returns non-zero error code, c remains untouched
+int cart_create(cart *c, FILE *f);
+void cart_free(cart *self);
+
+#endif

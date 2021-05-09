560336c1d8608ca76cd20912215de46f833fed15
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-09 16:34:02 -0700

    add memmap files

---
+++ src/emu/memorymap.c
@@ -0,0 +1,8 @@
+//
+//  memorymap.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 5/9/21.
+//
+
+#include "memorymap.h"
---
+++ src/emu/memorymap.h
@@ -0,0 +1,11 @@
+//
+//  memorymap.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 5/9/21.
+//
+
+#ifndef Aldo_emu_memorymap_h
+#define Aldo_emu_memorymap_h
+
+#endif

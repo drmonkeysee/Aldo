98d0eb7707f33b10c938e2a01dd84f470fdbe06a
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-09 17:05:40 -0700

    fill out memorymap interface

--- src/emu/memorymap.c
+++ src/emu/memorymap.c
@@ -6,3 +6,26 @@
 //
 
 #include "memorymap.h"
+
+#include <stdlib.h>
+
+// A Memory Map is a vector of Memory Banks
+
+struct memorymap {
+    size_t size, capacity;
+    struct memorybank *banks;
+};
+
+//
+// Public Interface
+//
+
+memmap *memmap_new(void)
+{
+    return NULL;
+}
+
+void memmap_free(memmap *self)
+{
+    free(self);
+}
--- src/emu/memorymap.h
+++ src/emu/memorymap.h
@@ -8,4 +8,35 @@
 #ifndef Aldo_emu_memorymap_h
 #define Aldo_emu_memorymap_h
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// A Memory Bank is a 16-bit address window on to some underlying memory space;
+// e.g. an 8KB bank from $B000 - $CFFF on an underlying 64KB PRG ROM space;
+// or a 2KB bank from $0000 - $7FFF with mask 0x800, mirroring $0000 - $07FF
+// through the full 2KB bank.
+
+struct memorybank {
+    size_t size;        // Bank Size
+    uint8_t *mem;       // Bank Memory
+    void *mapper;       // Mapper-owned Bank
+    uint16_t addrmin,   // Min Address covered by this bank
+             addrmax,   // Max Address covered by this bank
+             addrmask;  // Address mask applied to incoming addresses
+};
+
+typedef struct memorymap memmap;
+
+memmap *memmap_new(void);
+void memmap_free(memmap *self);
+
+bool memmap_add(memmap *self, size_t count,
+                const struct memorybank banks[count]);
+bool memmap_copy(memmap *self, size_t size, uint8_t buf[restrict size],
+                 uint16_t minaddr, uint16_t maxaddr);
+
+bool memmap_read(memmap *self, uint16_t addr, uint8_t *restrict d);
+bool memmap_write(memmap *self, uint16_t addr, uint8_t d);
+
 #endif

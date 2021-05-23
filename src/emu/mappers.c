61ae71960d48cd7807dc83e820f4e35651c1d395
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-22 17:24:07 -0700

    stub out mappers module

---
+++ src/emu/mappers.c
@@ -0,0 +1,8 @@
+//
+//  mappers.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 5/22/21.
+//
+
+#include "mappers.h"
---
+++ src/emu/mappers.h
@@ -0,0 +1,11 @@
+//
+//  mappers.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 5/22/21.
+//
+
+#ifndef Aldo_emu_mappers_h
+#define Aldo_emu_mappers_h
+
+#endif

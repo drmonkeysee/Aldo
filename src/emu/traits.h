139c076d7c6aae741dec67ee8dea462a9988ab32
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-05-29 21:40:37 -0700

    bitwidth constants

--- src/emu/traits.h
+++ src/emu/traits.h
@@ -10,6 +10,16 @@
 
 #include <stdint.h>
 
+// Bit Width Constants
+enum {
+    BITWIDTH_1KB = 10,
+    BITWIDTH_2KB = 11,
+    BITWIDTH_8KB = 13,
+    BITWIDTH_16KB = 14,
+    BITWIDTH_32KB = 15,
+    BITWIDTH_64KB = 16,
+};
+
 // CPU Memory Map
 #define NES_RAM_SIZE 0x800u     // 2 KB CPU RAM
 #define NES_ROM_SIZE 0x8000u    // 32 KB Fake ROM

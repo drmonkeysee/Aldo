d03ad4a77180a57cb770bd604b5355540cb1ac4a
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-06-09 21:25:01 -0700

    flesh out bank_copy tests

--- test/emu/bytes.c
+++ test/emu/bytes.c
@@ -8,7 +8,7 @@
 #include "ciny.h"
 #include "emu/bytes.h"
 
-static void bank_copy_zero(void *ctx)
+static void bank_copy_zero_count(void *ctx)
 {
     uint8_t bank[] = {
         0xaa, 0xaa, 0xaa,
@@ -22,6 +22,59 @@ static void bank_copy_zero(void *ctx)
     ct_assertequal(0x11u, dest);
 }
 
+static void bank_copy(void *ctx)
+{
+    uint8_t bank[] = {
+        0xaa, 0x99, 0x88, 0x77, 0x66,
+        [1021] = 0xff, [1022] = 0xff, [1023] = 0xff,
+    };
+    uint8_t dest[5];
+
+    const size_t result = bytescopy_bank(bank, 10, 0x0,
+                                         sizeof dest / sizeof dest[0], dest);
+
+    ct_assertequal(5u, result);
+    ct_assertequal(0xaau, dest[0]);
+    ct_assertequal(0x99u, dest[1]);
+    ct_assertequal(0x88u, dest[2]);
+    ct_assertequal(0x77u, dest[3]);
+    ct_assertequal(0x66u, dest[4]);
+}
+
+static void bank_copy_end_of_bank(void *ctx)
+{
+    uint8_t bank[] = {
+        0xaa, 0xaa, 0xaa,
+        [1021] = 0xff, [1022] = 0xff, [1023] = 0xff,
+    };
+    uint8_t dest[5] = {[1] = 0x11};
+
+    const size_t result = bytescopy_bank(bank, 10, 0x3ff,
+                                         sizeof dest / sizeof dest[0], dest);
+
+    ct_assertequal(1u, result);
+    ct_assertequal(0xffu, dest[0]);
+    ct_assertequal(0x11u, dest[1]);
+}
+
+static void bank_copy_fit_end_of_bank(void *ctx)
+{
+    uint8_t bank[] = {
+        0xaa, 0xaa, 0xaa,
+        [1021] = 0xff, [1022] = 0xff, [1023] = 0xee,
+    };
+    uint8_t dest[] = {0x11, 0x11, 0x11, 0x11, 0x11};
+
+    const size_t result = bytescopy_bank(bank, 10, 0x3fb,
+                                         sizeof dest / sizeof dest[0], dest);
+
+    ct_assertequal(5u, result);
+    ct_assertequal(0x0u, dest[0]);
+    ct_assertequal(0x0u, dest[1]);
+    ct_assertequal(0xffu, dest[2]);
+    ct_assertequal(0xffu, dest[3]);
+    ct_assertequal(0xeeu, dest[4]);
+}
 
 //
 // Test List
@@ -30,7 +83,10 @@ static void bank_copy_zero(void *ctx)
 struct ct_testsuite bytes_tests(void)
 {
     static const struct ct_testcase tests[] = {
-        ct_maketest(bank_copy_zero),
+        ct_maketest(bank_copy_zero_count),
+        ct_maketest(bank_copy),
+        ct_maketest(bank_copy_end_of_bank),
+        ct_maketest(bank_copy_fit_end_of_bank),
     };
 
     return ct_makesuite(tests);

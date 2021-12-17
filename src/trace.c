ee8b710dd20fddf3eab6639fb79c519987250773
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-12-16 19:14:17 -0800

    break up trace into simpler functions

--- src/trace.c
+++ src/trace.c
@@ -14,6 +14,59 @@
 #include <inttypes.h>
 #include <stddef.h>
 
+static int trace_instruction(FILE *tracelog, const struct mos6502 *cpu,
+                             const struct console_state *snapshot)
+{
+    uint8_t inst[3];
+    const size_t instlen = bus_dma(cpu->bus,
+                                   snapshot->datapath.current_instruction,
+                                   sizeof inst / sizeof inst[0], inst);
+    char disinst[DIS_INST_SIZE];
+    const int result = dis_inst(snapshot->datapath.current_instruction,
+                                inst, instlen, disinst);
+    if (result > 0) {
+        // NOTE: nestest compatibility:
+        //  - convert : to space
+        //disinst[4] = ' ';
+        return fprintf(tracelog, "%s", disinst);
+    } else {
+        return fprintf(tracelog, "%s",
+                       result < 0 ? dis_errstr(result) : "No inst");
+    }
+}
+
+static int trace_instruction_peek(FILE *tracelog, struct mos6502 *cpu,
+                                  const struct console_state *snapshot)
+{
+    char peek[DIS_PEEK_SIZE];
+    const int result = dis_peek(snapshot->datapath.current_instruction, cpu,
+                                snapshot, peek);
+    return fprintf(tracelog, " %s", result < 0 ? dis_errstr(result) : peek);
+}
+
+static void trace_registers(FILE *tracelog,
+                            const struct console_state *snapshot)
+{
+    static const char flags[] = {
+        'c', 'C', 'z', 'Z', 'i', 'I', 'd', 'D',
+        'b', 'B', '-', '-', 'v', 'V', 'n', 'N',
+    };
+
+    fprintf(tracelog, " A:%02X X:%02X Y:%02X P:%02X (",
+            snapshot->cpu.accumulator, snapshot->cpu.xindex,
+            snapshot->cpu.yindex, snapshot->cpu.status);
+    for (size_t i = sizeof snapshot->cpu.status * 8; i > 0; --i) {
+        const size_t idx = i - 1;
+        const bool bit = (snapshot->cpu.status >> idx) & 1;
+        fputc(flags[(idx * 2) + bit], tracelog);
+    }
+    fprintf(tracelog, ") SP:%02X", snapshot->cpu.stack_pointer);
+}
+
+//
+// Public Interface
+//
+
 // TODO: add nestest compatibility flag
 void trace_line(FILE *tracelog, uint64_t cycles, struct mos6502 *cpu)
 {
@@ -24,49 +77,14 @@ void trace_line(FILE *tracelog, uint64_t cycles, struct mos6502 *cpu)
     // NOTE: does not include leading space in registers print-format
     static const int instw = 47;
 
-    uint8_t inst[3];
     struct console_state snapshot;
     cpu_snapshot(cpu, &snapshot);
-    const size_t instlen = bus_dma(cpu->bus,
-                                   snapshot.datapath.current_instruction,
-                                   sizeof inst / sizeof inst[0], inst);
 
-    char disinst[DIS_INST_SIZE];
-    int result = dis_inst(snapshot.datapath.current_instruction, inst, instlen,
-                          disinst),
-        written = 0;
-    if (result > 0) {
-        // NOTE: nestest compatibility:
-        //  - convert : to space
-        //disinst[4] = ' ';
-        written += fprintf(tracelog, "%s", disinst);
-    } else {
-        written += fprintf(tracelog, "%s",
-                           result < 0 ? dis_errstr(result) : "No inst");
-    }
-
-    char peek[DIS_PEEK_SIZE];
-    result = dis_peek(snapshot.datapath.current_instruction, cpu, &snapshot,
-                      peek);
-    written += fprintf(tracelog, " %s",
-                       result < 0 ? dis_errstr(result) : peek);
-
-    const int width = written < 0 ? instw : (written > instw
-                                             ? 0
-                                             : instw - written);
+    const int
+        written = trace_instruction(tracelog, cpu, &snapshot)
+                    + trace_instruction_peek(tracelog, cpu, &snapshot),
+        width = written < 0 ? instw : (written > instw ? 0 : instw - written);
     fprintf(tracelog, "%*s", width, "");
-    fprintf(tracelog, " A:%02X X:%02X Y:%02X P:%02X (",
-            snapshot.cpu.accumulator, snapshot.cpu.xindex, snapshot.cpu.yindex,
-            snapshot.cpu.status);
-    static const char flags[] = {
-        'c', 'C', 'z', 'Z', 'i', 'I', 'd', 'D',
-        'b', 'B', '-', '-', 'v', 'V', 'n', 'N',
-    };
-    for (size_t i = sizeof snapshot.cpu.status * 8; i > 0; --i) {
-        const size_t idx = i - 1;
-        const bool bit = (snapshot.cpu.status >> idx) & 1;
-        fputc(flags[(idx * 2) + bit], tracelog);
-    }
-    fprintf(tracelog, ") SP:%02X", snapshot.cpu.stack_pointer);
+    trace_registers(tracelog, &snapshot);
     fprintf(tracelog, " CYC:%" PRIu64 "\n", cycles);
 }

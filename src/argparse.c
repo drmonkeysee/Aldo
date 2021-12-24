83d8efb036321de2acfd67f591b9d32c6afaeed8
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-12-23 17:34:10 -0800

    move argument parsing into its own module

---
+++ src/argparse.c
@@ -0,0 +1,226 @@
+//
+//  argparse.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 12/23/21.
+//
+
+#include "argparse.h"
+
+#include "bytes.h"
+
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const char
+    *const restrict Version = "0.2.1", // TODO: autogenerate this
+
+    *const restrict ChrDecodeLong = "--chr-decode",
+    *const restrict ChrScaleLong = "--chr-scale",
+    *const restrict DisassembleLong = "--disassemble",
+    *const restrict HaltLong = "--halt",
+    *const restrict HelpLong = "--help",
+    *const restrict InfoLong = "--info",
+    *const restrict ResVectorLong = "--reset-vector",
+    *const restrict TraceLong = "--trace",
+    *const restrict VersionLong = "--version";
+
+static const char
+    ChrDecodeShort = 'c',
+    ChrScaleShort = 's',
+    DisassembleShort = 'd',
+    HaltShort = 'H',
+    HelpShort = 'h',
+    InfoShort = 'i',
+    ResVectorShort = 'r',
+    TraceShort = 't',
+    VerboseShort = 'v',
+    VersionShort = 'V';
+
+static const int
+    MinScale = 1, MaxScale = 10, MinAddress = 0, MaxAddress = ADDRMASK_64KB;
+
+static void init_control(struct control *appstate)
+{
+    *appstate = (struct control){
+        .chrscale = MinScale,
+        .clock = {.cycles_per_sec = 4},
+        .haltaddr = -1,
+        .resetvector = -1,
+        .running = true,
+    };
+}
+
+static bool parse_flag(const char *arg, char shrt, bool exact, const char *lng)
+{
+    return (strlen(arg) > 1
+                && exact
+                    ? arg[1] == shrt
+                    : arg[1] != '-' && strchr(arg, shrt) != NULL)
+            || (lng && strcmp(arg, lng) == 0);
+}
+
+#define setflag(f, a, s, l) (f) = (f) || parse_flag(a, s, false, l)
+
+static bool convert_num(const char *arg, int base, long *restrict result)
+{
+    char *end;
+    errno = 0;
+    *result = strtol(arg, &end, base);
+    if (errno == ERANGE) {
+        perror("Number parse failed");
+        return false;
+    }
+    if (*result == 0 && arg == end) {
+        return false;
+    }
+    return true;
+}
+
+// NOTE: value of number is undefined if return value is false
+static bool parse_number(const char *arg, int *restrict argi, int argc,
+                         char *argv[argc+1], int base, long *restrict number)
+{
+    // NOTE: first try -sN format
+    bool result = convert_num(arg + 2, base, number);
+    // NOTE: then try --long-name=N format
+    if (!result && arg[1] == '-') {
+        const char *const opt = strchr(arg, '=');
+        if (opt) {
+            result = convert_num(opt + 1, base, number);
+        }
+    }
+    // NOTE: finally try parsing next argument
+    if (!result) {
+        result = ++*argi < argc
+                    ? convert_num(argv[*argi], base, number)
+                    : false;
+    }
+    return result;
+}
+
+static bool parse_address(const char *arg, int *restrict argi, int argc,
+                          char *argv[argc+1], const char *restrict label,
+                          int *restrict parsed)
+{
+    long addr;
+    const bool result = parse_number(arg, argi, argc, argv, 16, &addr);
+    if (result && MinAddress <= addr && addr <= MaxAddress) {
+        *parsed = (int)addr;
+    } else {
+        fprintf(stderr, "Invalid %s format: expected [0x%X, 0x%X]\n", label,
+                MinAddress, MaxAddress);
+        return false;
+    }
+    return true;
+}
+
+static bool parse_arg(struct control *restrict appstate, const char *arg,
+                      int *restrict argi, int argc, char *argv[argc+1])
+{
+    if (parse_flag(arg, ChrScaleShort, true, ChrScaleLong)) {
+        long scale;
+        const bool result = parse_number(arg, argi, argc, argv, 10, &scale);
+        if (result && MinScale <= scale && scale <= MaxScale) {
+            appstate->chrscale = (int)scale;
+        } else {
+            fprintf(stderr, "Invalid scale format: expected [%d, %d]\n",
+                    MinScale, MaxScale);
+            return false;
+        }
+        return true;
+    }
+
+    if (parse_flag(arg, HaltShort, true, HaltLong)) {
+        return parse_address(arg, argi, argc, argv, "address",
+                             &appstate->haltaddr);
+    }
+
+    if (parse_flag(arg, ResVectorShort, true, ResVectorLong)) {
+        return parse_address(arg, argi, argc, argv, "vector",
+                             &appstate->resetvector);
+    }
+
+    setflag(appstate->disassemble, arg, DisassembleShort, DisassembleLong);
+    setflag(appstate->help, arg, HelpShort, HelpLong);
+    setflag(appstate->info, arg, InfoShort, InfoLong);
+    setflag(appstate->tron, arg, TraceShort, TraceLong);
+    setflag(appstate->verbose, arg, VerboseShort, NULL);
+    setflag(appstate->version, arg, VersionShort, VersionLong);
+
+    setflag(appstate->chrdecode, arg, ChrDecodeShort, ChrDecodeLong);
+    const size_t chroptlen = strlen(ChrDecodeLong);
+    if (strncmp(arg, ChrDecodeLong, chroptlen) == 0) {
+        const char *const opt = strchr(arg, '=');
+        if (opt && opt - arg == chroptlen) {
+            appstate->chrdecode_prefix = opt + 1;
+        }
+    }
+    return true;
+}
+
+//
+// Public Interface
+//
+
+bool argparse_parse(struct control *restrict appstate, int argc,
+                    char *argv[argc+1])
+{
+    init_control(appstate);
+    appstate->me = argc > 0 && strlen(argv[0]) > 0 ? argv[0] : "aldo";
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            const char *const arg = argv[i];
+            if (arg[0] == '-') {
+                if (!parse_arg(appstate, arg, &i, argc, argv)) return false;
+            } else {
+                appstate->cartfile = arg;
+            }
+        }
+    } else {
+        appstate->help = true;
+    }
+    return true;
+}
+
+void argparse_usage(const struct control *appstate)
+{
+    puts("---=== Aldo Usage ===---");
+    printf("%s [options...] [command] file\n", appstate->me);
+    puts("\noptions");
+    printf("  -%c x\t: HALT when PC reaches this address [0x%X, 0x%X]"
+           " (also %s x)\n", HaltShort, MinAddress, MaxAddress, HaltLong);
+    printf("  -%c x\t: override RESET vector [0x%X, 0x%X]"
+           " (also %s x)\n", ResVectorShort, MinAddress, MaxAddress,
+           ResVectorLong);
+    printf("  -%c n\t: CHR ROM BMP scaling factor [%d, %d]"
+           " (also %s n)\n", ChrScaleShort, MinScale, MaxScale, ChrScaleLong);
+    printf("  -%c\t: turn on trace-logging and ram dumps (also %s)\n",
+           TraceShort, TraceLong);
+    printf("  -%c\t: verbose output\n", VerboseShort);
+    puts("\ncommands");
+    printf("  -%c\t: decode CHR ROM into BMP files (also %s[=prefix];"
+           " prefix default is 'bank')\n", ChrDecodeShort, ChrDecodeLong);
+    printf("  -%c\t: disassemble file (also %s);"
+           " verbose prints duplicate lines\n", DisassembleShort,
+           DisassembleLong);
+    printf("  -%c\t: print usage (also %s)\n", HelpShort, HelpLong);
+    printf("  -%c\t: print file cartridge info (also %s);"
+           " verbose prints more details\n", InfoShort, InfoLong);
+    printf("  -%c\t: print version (also %s)\n", VersionShort, VersionLong);
+    puts("\narguments");
+    puts("  file\t: input file containing cartridge"
+         " or program contents");
+}
+
+void argparse_version(void)
+{
+    printf("Aldo %s", Version);
+#ifdef __VERSION__
+    fputs(" (" __VERSION__ ")", stdout);
+#endif
+    puts("");
+}
---
+++ src/argparse.h
@@ -0,0 +1,20 @@
+//
+//  argparse.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 12/23/21.
+//
+
+#ifndef Aldo_argparse_h
+#define Aldo_argparse_h
+
+#include "control.h"
+
+#include <stdbool.h>
+
+bool argparse_parse(struct control *restrict appstate, int argc,
+                    char *argv[argc+1]);
+void argparse_usage(const struct control *appstate);
+void argparse_version(void);
+
+#endif

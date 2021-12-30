1791f5776273f722e3afe1edefab4783dfc835fd
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-12-29 19:53:50 -0800

    stub out debug module

---
+++ src/debug.c
@@ -0,0 +1,8 @@
+//
+//  debug.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 12/29/21.
+//
+
+#include "debug.h"
---
+++ src/debug.h
@@ -0,0 +1,11 @@
+//
+//  debug.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 12/29/21.
+//
+
+#ifndef Aldo_debug_h
+#define Aldo_debug_h
+
+#endif

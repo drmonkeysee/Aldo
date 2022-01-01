1a35e7567772fed8fe1b2091ee912df38396c366
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-12-31 23:59:17 -0800

    implement cli spinner

--- src/uibatch.c
+++ src/uibatch.c
@@ -14,6 +14,28 @@
 #include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+static const double MillisecondsPerSecond = 1000,
+                    NanosecondsPerMillisecond = 1e6;
+static const char *const restrict DistractorFormat = "%c Still running\u2026";
+
+static struct timespec Current, Previous;
+static double FrameTimeMs;
+
+static double to_ms(const struct timespec *ts)
+{
+    return (ts->tv_sec * MillisecondsPerSecond)
+            + (ts->tv_nsec / NanosecondsPerMillisecond);
+}
+
+static void clearline(void)
+{
+    for (size_t i = 0; i < strlen(DistractorFormat) * 2; ++i) {
+        fputc('\b', stderr);
+    }
+}
 
 static volatile sig_atomic_t QuitSignal;
 
@@ -29,6 +51,7 @@ static void handle_sigint(int sig, siginfo_t *info, void *uap)
 
 static int batch_init(void)
 {
+    clock_gettime(CLOCK_MONOTONIC, &Previous);
     struct sigaction act = {
         .sa_sigaction = handle_sigint,
         .sa_flags = SA_SIGINFO,
@@ -39,6 +62,9 @@ static int batch_init(void)
 static void batch_tick_start(struct control *appstate,
                              const struct console_state *snapshot)
 {
+    clock_gettime(CLOCK_MONOTONIC, &Current);
+    FrameTimeMs = to_ms(&Current) - to_ms(&Previous);
+
     // NOTE: 1 cycle per tick approximates running emu at native cpu speed
     appstate->clock.budget = 1;
 
@@ -50,7 +76,7 @@ static void batch_tick_start(struct control *appstate,
 
 static void batch_tick_end(void)
 {
-    puts("Batch tick end");
+    Previous = Current;
 }
 
 static int batch_pollinput(void)
@@ -61,12 +87,25 @@ static int batch_pollinput(void)
 static void batch_refresh(const struct control *appstate,
                           const struct console_state *snapshot)
 {
-    puts("Batch refresh");
+    static const char distractor[] = {'|', '/', '-', '\\'};
+    static const double display_wait = 2000,
+                        refresh_interval_ms = display_wait + 250;
+    static double refreshdt;
+    static size_t distractor_frame;
+
+    (void)appstate, (void)snapshot;
+    if ((refreshdt += FrameTimeMs) >= refresh_interval_ms) {
+        refreshdt = display_wait;
+        clearline();
+        fprintf(stderr, DistractorFormat, distractor[distractor_frame++]);
+        distractor_frame %= sizeof distractor;
+    }
 }
 
 static void batch_cleanup(const struct control *appstate,
                           const struct console_state *snapshot)
 {
+    clearline();
     if (snapshot->debugger.halt_address >= 0) {
         printf("Halted @: %04X\n", snapshot->debugger.halt_address);
     }

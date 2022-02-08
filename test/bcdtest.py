904785ec2a0964b0705ef895456bebc62d9741e2
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-02-07 21:21:35 -0800

    add reset vector to rom output

--- test/bcdtest.py
+++ test/bcdtest.py
@@ -1,20 +1,23 @@
 """Convert bcdtest PRG file into a 32KB ROM file."""
 from pathlib import Path
 
+RESET_VECTOR = 0xfffc - 0x8000
+START_OFFSET = 0x600
+
 
 def main():
     folder = Path(__file__).parent
     with open(folder / 'bcdtest.out', 'rb') as f:
         prg = f.read()
     padding = [0xff]
-    preamble = padding * 0x600
+    preamble = bytes(padding * START_OFFSET)
+    bank_fill = bytes(padding * (2**15 - len(prg) - len(preamble)))
     rom_file = 'bcdtest.rom'
+    prg_bank = bytearray(preamble + prg + bank_fill)
+    prg_bank[RESET_VECTOR:RESET_VECTOR + 2] = [0x0, 0x86]
     with open(folder / rom_file, 'wb') as f:
-        f.write(
-            bytes(preamble)
-            + prg
-            + bytes(padding * (2**15 - len(prg) - len(preamble)))
-        )
+        count = f.write(prg_bank)
+    assert count == 2**15
     print('Generated ROM file', rom_file)
 
 

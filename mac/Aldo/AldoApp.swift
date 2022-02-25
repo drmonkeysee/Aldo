11113cd71e8b07b3ffd1d3c15ca62ae4e262df1c
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-02-24 18:06:06 -0800

    add mac app target

---
+++ mac/Aldo/AldoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AldoApp.swift
+//  Aldo
+//
+//  Created by Brandon Stansbury on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AldoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac/Aldo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aldo
+//
+//  Created by Brandon Stansbury on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

f83404dd7c19ea75c1e4fddadc25cd3e8f1350ab
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-03-05 21:44:21 -0800

    auto-select first nav link

--- mac/Aldo/ContentView.swift
+++ mac/Aldo/ContentView.swift
@@ -11,24 +11,29 @@ struct ContentView: View {
     static let fileLabel = "Open ROM File"
 
     @State private var fileUrl: URL?
+    @State private var navSelection: Int? = 0
 
     var body: some View {
         NavigationView {
             List {
                 NavigationLink("Aldo",
                                destination: EmulatorView()
-                                .navigationTitle("Aldo"))
+                                .navigationTitle("Aldo"),
+                               tag: 0, selection: $navSelection)
                 NavigationLink("Breadboard",
                                destination: BreadboardView()
-                                .navigationTitle("Breadboard"))
+                                .navigationTitle("Breadboard"),
+                               tag: 1, selection: $navSelection)
                 NavigationLink("Asm",
                                destination: AssemblerView()
-                                .navigationTitle("Assembler"))
+                                .navigationTitle("Assembler"),
+                               tag: 2, selection: $navSelection)
                 NavigationLink("Details",
                                destination: CartDetailsView(fileUrl: $fileUrl)
                                 .padding(EdgeInsets(top: 0, leading: 5,
                                                     bottom: 5, trailing: 5))
-                                .navigationTitle("Cart Details"))
+                                .navigationTitle("Cart Details"),
+                               tag: 3, selection: $navSelection)
 
             }
         }

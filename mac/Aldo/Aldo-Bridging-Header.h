1767e07a9100734bfbd1c96a0956156f46d96684
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-03-16 22:17:35 -0700

    wire cart up to aldo app, stub out handle and err codes

--- mac/Aldo/Aldo-Bridging-Header.h
+++ mac/Aldo/Aldo-Bridging-Header.h
@@ -2,3 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#define CART_ERRCODE_BRIDGE_NAME CartErrCode
+#include "cart.h"
--- mac/Aldo/ViewModels/Cart.swift
+++ mac/Aldo/ViewModels/Cart.swift
@@ -10,6 +10,8 @@ import Foundation
 final class Cart: ObservableObject {
     @Published private(set) var file: URL?
     @Published private(set) var info: CartInfo = .none
+    @Published private(set) var error: CartErrCode?
+    private var handle: CartHandle?
 
     var name: String? { file?.lastPathComponent }
 
@@ -25,4 +27,17 @@ enum CartInfo {
     case raw
 
     var name: String { "\(self)".capitalized }
+}
+
+final class CartHandle {
+    private let handle: UnsafeRawPointer?
+
+    init?(fromFile: URL) {
+        // open file
+        // if fails set error
+        // open cart
+        // if fails set error
+        // close file
+        nil
+    }
 }

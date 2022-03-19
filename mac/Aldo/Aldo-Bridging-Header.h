d8d160669f735ef23d224c89ddd8e560195a9126
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-03-18 21:02:42 -0700

    load cart into handle and set error cases

--- mac/Aldo/Aldo-Bridging-Header.h
+++ mac/Aldo/Aldo-Bridging-Header.h
@@ -2,5 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#define CART_ERRCODE_BRIDGE_NAME CartErrCode
 #include "cart.h"
--- mac/Aldo/ViewModels/Cart.swift
+++ mac/Aldo/ViewModels/Cart.swift
@@ -9,8 +9,8 @@ import Foundation
 
 final class Cart: ObservableObject {
     @Published private(set) var file: URL?
-    @Published private(set) var info: CartInfo = .none
-    @Published private(set) var error: CartErrCode?
+    @Published private(set) var info = CartInfo.none
+    @Published private(set) var loadError: CartError?
     private var handle: CartHandle?
 
     var name: String? { file?.lastPathComponent }
@@ -18,6 +18,13 @@ final class Cart: ObservableObject {
     func load(from: URL?) {
         guard let filePath = from else { return }
         file = filePath
+        do {
+            handle = try CartHandle(fromFile: file!)
+        } catch let err as CartError {
+            loadError = err
+        } catch {
+            loadError = CartError.unknown
+        }
         info = .raw
     }
 }
@@ -29,15 +36,43 @@ enum CartInfo {
     var name: String { "\(self)".capitalized }
 }
 
-final class CartHandle {
-    private let handle: UnsafeRawPointer?
+enum CartError: Error {
+    case unknown
+    case ioError(String)
+    case errCode(Int32)
 
-    init?(fromFile: URL) {
-        // open file
-        // if fails set error
-        // open cart
-        // if fails set error
-        // close file
-        nil
+    func message() -> String {
+        switch self {
+        case .unknown:
+            return "Unknown error"
+        case .ioError(let str):
+            return str
+        case .errCode(let code):
+            return String(cString: cart_errstr(code))
+        }
+    }
+}
+
+fileprivate final class CartHandle {
+    private var handle: OpaquePointer? = nil
+
+    init(fromFile: URL) throws {
+        errno = 0
+        guard let cFile = fromFile.withUnsafeFileSystemRepresentation({
+            fopen($0, "rb")
+        }) else {
+            throw CartError.ioError(String(cString: strerror(errno)))
+        }
+
+        defer { fclose(cFile) }
+        let err = cart_create(&handle, cFile)
+        guard err == 0 else {
+            throw CartError.errCode(err)
+        }
+    }
+
+    deinit {
+        cart_free(self.handle)
+        self.handle = nil
     }
 }

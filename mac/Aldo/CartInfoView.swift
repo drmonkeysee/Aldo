2c08340fb174ec4ab780f3515024040acb17ac21
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-06-18 15:43:21 -0700

    make cart info text selectable

--- mac/Aldo/CartInfoView.swift
+++ mac/Aldo/CartInfoView.swift
@@ -15,7 +15,9 @@ struct CartInfoView: View {
             HStack(alignment: .top) {
                 VStack(alignment: .trailing) {
                     CommonInfoView(section: .labels)
+                        .textSelection(.enabled)
                     FormatView(section: .labels, info: cart.info)
+                        .textSelection(.enabled)
                 }
                 VStack {
                     CommonInfoView(section: .separators)
@@ -23,7 +25,9 @@ struct CartInfoView: View {
                 }
                 VStack(alignment: .leading) {
                     CommonInfoView(section: .values)
+                        .textSelection(.enabled)
                     FormatView(section: .values, info: cart.info)
+                        .textSelection(.enabled)
                 }
                 switch cart.info {
                 case .raw, .iNes:

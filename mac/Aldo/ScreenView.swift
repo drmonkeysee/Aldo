eda86224f3e8514ec267c494933810bb8d494ecd
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-09-16 19:57:46 -0700

    wire up simple scene to spritekit view

--- mac/Aldo/ScreenView.swift
+++ mac/Aldo/ScreenView.swift
@@ -5,11 +5,14 @@
 //  Created by Brandon Stansbury on 3/9/22.
 //
 
+import SpriteKit
 import SwiftUI
 
 struct ScreenView: View {
     private static let nesResolution = (w: 256, h: 240)
     private static let nesScale = 2
+
+    @State private var scene = EmulatorScene()
 
     var body: some View {
         ZStack {
@@ -17,11 +20,29 @@ struct ScreenView: View {
                 .cornerRadius(5)
                 .frame(width: .init(Self.nesResolution.w * Self.nesScale),
                        height: .init(Self.nesResolution.h * Self.nesScale))
-            Color.cyan
+            SpriteView(scene: scene,
+                       debugOptions: [.showsFPS,
+                                      .showsNodeCount,
+                                      .showsDrawCount,
+                                      .showsQuadCount])
                 .frame(width: .init(Self.nesResolution.w),
                        height: .init(Self.nesResolution.h))
             Text("Emu Screen")
         }
+    }
+}
+
+fileprivate class EmulatorScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.size = view.bounds.size
+        self.backgroundColor = .cyan
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        self.removeAllChildren()
+        let block = SKSpriteNode(color: .yellow,
+                                 size: .init(width: 100, height: 100))
+        self.addChild(block)
     }
 }
 

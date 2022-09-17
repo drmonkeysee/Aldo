e832f9ee539793df620cfdc451cc3efb2c842e99
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-09-16 20:31:42 -0700

    animate the test block

--- mac/Aldo/ScreenView.swift
+++ mac/Aldo/ScreenView.swift
@@ -34,17 +34,35 @@ struct ScreenView: View {
     }
 }
 
-fileprivate class EmulatorScene: SKScene {
+fileprivate final class EmulatorScene: SKScene {
+    private static let blockHalfDim = 25.0
+    private static let blockDim = blockHalfDim * 2
+
+    private let block = SKSpriteNode(color: .yellow,
+                                     size: .init(
+                                        width: EmulatorScene.blockDim,
+                                        height: EmulatorScene.blockDim))
+    private var velocity: CGPoint = .init(x: 1, y: 1)
+
     override func didMove(to view: SKView) {
         self.size = view.bounds.size
         self.backgroundColor = .cyan
+        self.block.position = .init(x: Self.blockHalfDim,
+                                    y: Self.blockHalfDim)
+        self.addChild(self.block)
     }
 
     override func update(_ currentTime: TimeInterval) {
-        self.removeAllChildren()
-        let block = SKSpriteNode(color: .yellow,
-                                 size: .init(width: 100, height: 100))
-        self.addChild(block)
+        self.block.position.x += self.velocity.x
+        if self.block.position.x - Self.blockHalfDim < 0
+            || self.block.position.x + Self.blockHalfDim >= self.size.width {
+            self.velocity.x = -self.velocity.x
+        }
+        self.block.position.y += self.velocity.y
+        if self.block.position.y - Self.blockHalfDim < 0
+            || self.block.position.y + Self.blockHalfDim >= self.size.height {
+            self.velocity.y = -self.velocity.y
+        }
     }
 }
 

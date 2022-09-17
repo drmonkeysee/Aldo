4d915b08b7d8b83ba6f62d036c9d32b65d4c35ba
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-09-17 12:01:07 -0700

    add startup weight and move block to center of view

--- mac/Aldo/ScreenView.swift
+++ mac/Aldo/ScreenView.swift
@@ -37,22 +37,28 @@ struct ScreenView: View {
 fileprivate final class EmulatorScene: SKScene {
     private static let blockHalfDim = 25.0
     private static let blockDim = blockHalfDim * 2
+    private static let waitFrames = 120
 
     private let block = SKSpriteNode(color: .yellow,
                                      size: .init(
                                         width: EmulatorScene.blockDim,
                                         height: EmulatorScene.blockDim))
     private var velocity: CGPoint = .init(x: 1, y: 1)
+    private var frameCount = 0
 
     override func didMove(to view: SKView) {
         self.size = view.bounds.size
         self.backgroundColor = .cyan
-        self.block.position = .init(x: Self.blockHalfDim,
-                                    y: Self.blockHalfDim)
+        self.block.position = .init(x: self.size.width / 2,
+                                    y: self.size.height / 2)
         self.addChild(self.block)
     }
 
     override func update(_ currentTime: TimeInterval) {
+        if self.frameCount < Self.waitFrames {
+            self.frameCount += 1
+            return
+        }
         self.block.position.x += self.velocity.x
         if self.block.position.x - Self.blockHalfDim < 0
             || self.block.position.x + Self.blockHalfDim >= self.size.width {

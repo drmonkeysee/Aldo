10de7a55164b436bba12c27cf298066b0761ec1c
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-09-23 23:41:34 -0700

    add objc aldo app target

---
+++ mac/Aldo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 9/23/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+

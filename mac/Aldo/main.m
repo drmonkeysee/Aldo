55076bbf4da1ccc0579ac688f910d029151fa3c3
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-09-24 17:45:39 -0700

    remove boilerplate, wire up sdl hello world

--- mac/Aldo/main.m
+++ mac/Aldo/main.m
@@ -5,12 +5,48 @@
 //  Created by Brandon Stansbury on 9/23/22.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-int main(int argc, const char *argv[argc+1])
+#include <SDL2/SDL.h>
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[argc+1])
 {
+    (void)argv;
     @autoreleasepool {
         NSLog(@"Aldo GUI started...");
     }
-    return NSApplicationMain(argc, argv);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL initialization failure: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    int status = EXIT_SUCCESS;
+    SDL_Window *const window = SDL_CreateWindow("Aldo",
+                                                SDL_WINDOWPOS_UNDEFINED,
+                                                SDL_WINDOWPOS_UNDEFINED,
+                                                800, 600, 0);
+    if (!window) {
+        fprintf(stderr, "SDL window creation failure: %s\n", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto exit_sdl;
+    }
+    SDL_Surface *const surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xff, 0xff, 0xff));
+    SDL_UpdateWindowSurface(window);
+    SDL_Event ev;
+    bool running = true;
+    do {
+        SDL_PollEvent(&ev);
+        if (ev.type == SDL_QUIT) {
+            running = false;
+        }
+    } while (running);
+
+    SDL_DestroyWindow(window);
+exit_sdl:
+    SDL_Quit();
+    return status;
 }

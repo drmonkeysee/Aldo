409b01098b28756d9e349b06771f5f10c2b51ba6
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-10-01 20:56:11 -0700

    remove non-trivial media runtime members into cpp

---
+++ src/gui/mediaruntime.cpp
@@ -0,0 +1,98 @@
+//
+//  mediaruntime.cpp
+//  Aldo
+//
+//  Created by Brandon Stansbury on 10/1/22.
+//
+
+#include "mediaruntime.hpp"
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
+
+#include <stdexcept>
+
+namespace
+{
+
+auto create_window(const aldo::aldo_guiopts& options,
+                   SDL_Point windowSize) noexcept
+{
+   const Uint32 flags = options.hi_dpi ? SDL_WINDOW_ALLOW_HIGHDPI : 0;
+   return SDL_CreateWindow("Aldo",
+                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                           windowSize.x, windowSize.y,
+                           flags);
+}
+
+auto create_renderer(const aldo::WindowHandle& hwin) noexcept
+{
+    const Uint32 flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    return SDL_CreateRenderer(hwin.raw(), -1, flags);
+}
+
+}
+
+//
+// Public Interface
+//
+
+aldo::SdlLib::SdlLib()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
+                        "SDL initialization failure: %s", SDL_GetError());
+        throw std::runtime_error{"SDL runtime failure"};
+    }
+}
+
+aldo::SdlLib::~SdlLib()
+{
+    SDL_Quit();
+}
+
+aldo::WindowHandle::WindowHandle(const aldo::aldo_guiopts& options,
+                                 SDL_Point windowSize)
+: win{create_window(options, windowSize)}
+{
+    if (!this->win) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
+                        "SDL window creation failure: %s", SDL_GetError());
+        throw std::runtime_error{"SDL window creation failure"};
+    }
+}
+
+aldo::RendererHandle::RendererHandle(const aldo::aldo_guiopts& options,
+                                     const aldo::WindowHandle& hwin)
+: ren{create_renderer(hwin)}
+{
+    if (!this->ren) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
+                        "SDL renderer creation failure: %s",
+                        SDL_GetError());
+        throw std::runtime_error{"SDL renderer creation failure"};
+    }
+    SDL_RenderSetScale(raw(),
+                       options.render_scale_factor,
+                       options.render_scale_factor);
+    SDL_RendererInfo info;
+    SDL_GetRendererInfo(raw(), &info);
+    SDL_Log("Render name: %s (%04X)", info.name, info.flags);
+}
+
+aldo::DearImGuiLib::DearImGuiLib(const aldo::WindowHandle& hwin,
+                                 const aldo::RendererHandle& hren)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForSDLRenderer(hwin.raw(), hren.raw());
+    ImGui_ImplSDLRenderer_Init(hren.raw());
+}
+
+aldo::DearImGuiLib::~DearImGuiLib()
+{
+    ImGui_ImplSDLRenderer_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+}
--- src/gui/mediaruntime.hpp
+++ src/gui/mediaruntime.hpp
@@ -10,13 +10,9 @@
 
 #include "options.h"
 
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_sdlrenderer.h"
 #include <SDL2/SDL.h>
 
 #include <memory>
-#include <stdexcept>
 #include <type_traits>
 
 namespace aldo
@@ -27,103 +23,40 @@ using sdl_deleter = std::integral_constant<std::decay_t<decltype(d)>, d>;
 
 class SdlLib final {
 public:
-    SdlLib()
-    {
-        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-            SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-                            "SDL initialization failure: %s", SDL_GetError());
-            throw std::runtime_error{"SDL runtime failure"};
-        }
-    }
-
+    SdlLib();
     SdlLib(const SdlLib&) = delete;
     SdlLib& operator=(const SdlLib&) = delete;
-
-    ~SdlLib() { SDL_Quit(); }
+    ~SdlLib();
 };
 
 class WindowHandle final {
 public:
-    WindowHandle(const aldo::aldo_guiopts& options, SDL_Point windowSize)
-    : win{createWindow(options, windowSize)}
-    {
-        if (!this->win) {
-            SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-                            "SDL window creation failure: %s", SDL_GetError());
-            throw std::runtime_error{"SDL window creation failure"};
-        }
-    }
+    WindowHandle(const aldo::aldo_guiopts& options, SDL_Point windowSize);
 
     SDL_Window* raw() const noexcept { return win.get(); }
 
 private:
-    static SDL_Window* createWindow(const aldo::aldo_guiopts& options,
-                                     SDL_Point windowSize) noexcept
-   {
-       const Uint32 flags = options.hi_dpi ? SDL_WINDOW_ALLOW_HIGHDPI : 0;
-       return SDL_CreateWindow("Aldo",
-                               SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                               windowSize.x, windowSize.y,
-                               flags);
-   }
-
     std::unique_ptr<SDL_Window, aldo::sdl_deleter<SDL_DestroyWindow>> win;
 };
 
 class RendererHandle final {
 public:
     RendererHandle(const aldo::aldo_guiopts& options,
-                   const aldo::WindowHandle& hwin)
-    : ren{createRenderer(hwin)}
-    {
-        if (!this->ren) {
-            SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-                            "SDL renderer creation failure: %s",
-                            SDL_GetError());
-            throw std::runtime_error{"SDL renderer creation failure"};
-        }
-        SDL_RenderSetScale(raw(),
-                           options.render_scale_factor,
-                           options.render_scale_factor);
-        SDL_RendererInfo info;
-        SDL_GetRendererInfo(raw(), &info);
-        SDL_Log("Render name: %s (%X)", info.name, info.flags);
-    }
+                   const aldo::WindowHandle& hwin);
 
     SDL_Renderer* raw() const noexcept { return ren.get(); }
 
 private:
-    static SDL_Renderer*
-    createRenderer(const aldo::WindowHandle& hwin) noexcept
-    {
-        const Uint32 flags = SDL_RENDERER_ACCELERATED
-                                | SDL_RENDERER_PRESENTVSYNC;
-        return SDL_CreateRenderer(hwin.raw(), -1, flags);
-    }
-
     std::unique_ptr<SDL_Renderer, aldo::sdl_deleter<SDL_DestroyRenderer>> ren;
 };
 
 class DearImGuiLib final {
 public:
     DearImGuiLib(const aldo::WindowHandle& hwin,
-                 const aldo::RendererHandle& hren)
-    {
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        ImGui_ImplSDL2_InitForSDLRenderer(hwin.raw(), hren.raw());
-        ImGui_ImplSDLRenderer_Init(hren.raw());
-    }
-
+                 const aldo::RendererHandle& hren);
     DearImGuiLib(const DearImGuiLib&) = delete;
     DearImGuiLib& operator=(const DearImGuiLib&) = delete;
-
-    ~DearImGuiLib()
-    {
-        ImGui_ImplSDLRenderer_Shutdown();
-        ImGui_ImplSDL2_Shutdown();
-        ImGui::DestroyContext();
-    }
+    ~DearImGuiLib();
 };
 
 class MediaRuntime final {

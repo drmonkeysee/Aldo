e4e5a023a60572ccbd5dbb4637370309af46ddfa
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-10-22 19:44:42 -0700

    add bundled cli target

---
+++ mac/Cli/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cli
+//
+//  Created by Brandon Stansbury on 10/22/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

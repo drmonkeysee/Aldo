43a83db5090db42a61387a26b1e9682db62cdf0d
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-10-22 19:52:55 -0700

    update cli build settings

--- mac/Cli/main.c
+++ mac/Cli/main.c
@@ -1,13 +1,14 @@
 //
 //  main.c
-//  Cli
+//  Aldo-Cli
 //
 //  Created by Brandon Stansbury on 10/22/22.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char *argv[argc+1])
+{
     // insert code here...
     printf("Hello, World!\n");
     return 0;

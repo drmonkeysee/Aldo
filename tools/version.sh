2472cec59035880ea2fb927c42189e43d88ed247
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-10-28 22:47:59 -0700

    consistent brace usage

--- tools/version.sh
+++ tools/version.sh
@@ -20,14 +20,14 @@ if [ -n "$date" ] ; then
 fi
 build_no=$(date "${input_date[@]}" '+%Y.%j')
 
-echo "Setting version $ver ($build_no) (lib-compat: $major_ver)..."
+echo "Setting version $ver (${build_no}) (lib-compat: ${major_ver})..."
 
-sed -i .bak "s/\(AldoVersion = \)\".*\"/\1\"$ver ($build_no)\"/" ${PWD}/src/version.c
+sed -i .bak "s/\(AldoVersion = \)\".*\"/\1\"${ver} (${build_no})\"/" ${PWD}/src/version.c
 echo 'Updated c-src version'
 
 sed -i .bak -E \
-	-e "s/(MARKETING|DYLIB_CURRENT)(_VERSION = ).*;/\1\2$ver;/" \
-	-e "s/(CURRENT_PROJECT_VERSION = ).*;/\1$build_no;/" \
-	-e "s/(DYLIB_COMPATIBILITY_VERSION = ).*;/\1$major_ver;/" \
+	-e "s/(MARKETING|DYLIB_CURRENT)(_VERSION = ).*;/\1\2${ver};/" \
+	-e "s/(CURRENT_PROJECT_VERSION = ).*;/\1${build_no};/" \
+	-e "s/(DYLIB_COMPATIBILITY_VERSION = ).*;/\1${major_ver};/" \
 	${PWD}/mac/Aldo.xcodeproj/project.pbxproj
 echo 'Updated mac project versions'

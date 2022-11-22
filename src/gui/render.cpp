077e92e3f5dc386240b3a7a502c3fe717aad5795
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-11-21 20:54:35 -0800

    wire snapshot up to imgui

--- src/gui/render.cpp
+++ src/gui/render.cpp
@@ -7,6 +7,9 @@
 
 #include "render.hpp"
 
+#include "bytes.h"
+#include "cart.h"
+#include "dis.h"
 #include "mediaruntime.hpp"
 #include "viewstate.hpp"
 
@@ -14,10 +17,20 @@
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_sdlrenderer.h"
 
-#include <array>
+#include <locale>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
+
+namespace
+{
+
+constexpr auto bool_to_linestate(bool v) noexcept
+{
+    return v ? "HI" : "LO";
+}
+
+}
 
 //
 // Public Interface
@@ -134,13 +147,11 @@ void aldo::RenderFrame::renderHardwareTraits() const noexcept
 void aldo::RenderFrame::renderCart() const noexcept
 {
     if (ImGui::Begin("Cart")) {
-        ImGui::TextUnformatted("Name:");
-        ImGui::SameLine();
-        ImGui::TextUnformatted("test rom");
-
-        ImGui::TextUnformatted("Format:");
-        ImGui::SameLine();
-        ImGui::TextUnformatted("unknown");
+        const auto& cart = state.snapshot.cart;
+        ImGui::Text("Name: %s", cart_filename(cart.info));
+        char cartFormat[CART_FMT_SIZE];
+        const auto result = cart_format_extname(cart.info, cartFormat);
+        ImGui::Text("Format: %s", result > 0 ? cartFormat : "Invalid Format");
     }
     ImGui::End();
 }
@@ -183,36 +194,21 @@ void aldo::RenderFrame::renderCpu() const noexcept
     if (!state.showCpu) return;
 
     if (ImGui::Begin("CPU", &state.showCpu)) {
+        const auto& cpu = state.snapshot.cpu;
         if (ImGui::CollapsingHeader("Registers")) {
             ImGui::BeginGroup();
             {
-                ImGui::TextUnformatted("A:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FF");
-
-                ImGui::TextUnformatted("X:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FF");
-
-                ImGui::TextUnformatted("Y:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FF");
+                ImGui::Text("A: %02X", cpu.accumulator);
+                ImGui::Text("X: %02X", cpu.xindex);
+                ImGui::Text("Y: %02X", cpu.yindex);
             }
             ImGui::EndGroup();
             ImGui::SameLine(125);
             ImGui::BeginGroup();
             {
-                ImGui::TextUnformatted("PC:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FFFF");
-
-                ImGui::TextUnformatted(" S:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FF");
-
-                ImGui::TextUnformatted(" P:");
-                ImGui::SameLine();
-                ImGui::TextUnformatted("FF");
+                ImGui::Text("PC: %04X", cpu.program_counter);
+                ImGui::Text(" S: %02X", cpu.stack_pointer);
+                ImGui::Text(" P: %02X", cpu.status);
             }
             ImGui::EndGroup();
         }
@@ -225,7 +221,6 @@ void aldo::RenderFrame::renderCpu() const noexcept
             flagOff = IM_COL32(0x43, 0x39, 0x36, SDL_ALPHA_OPAQUE),
             textOn = IM_COL32_BLACK,
             textOff = IM_COL32_WHITE;
-        static constexpr std::uint8_t status = 0x34;
         const auto textSize = ImGui::CalcTextSize("A");
         const auto radius = (textSize.x + textSize.y) / 2.0f;
         if (ImGui::CollapsingHeader("Flags")) {
@@ -238,7 +233,7 @@ void aldo::RenderFrame::renderCpu() const noexcept
             const auto drawList = ImGui::GetWindowDrawList();
             for (std::size_t i = 0; i < sizeof flags; ++i) {
                 ImU32 fill, text;
-                if (status & (1 << (sizeof flags - 1 - i))) {
+                if (cpu.status & (1 << (sizeof flags - 1 - i))) {
                     fill = flagOn;
                     text = textOn;
                 } else {
@@ -254,67 +249,105 @@ void aldo::RenderFrame::renderCpu() const noexcept
         }
 
         if (ImGui::CollapsingHeader("Datapath")) {
-            ImGui::TextUnformatted("Address Bus:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("01FB");
-            ImGui::TextUnformatted("Data Bus:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("00");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("R/W");
+            const auto& datapath = state.snapshot.datapath;
+            const auto& lines = state.snapshot.lines;
+
+            ImGui::Text("Address Bus: %04X", datapath.addressbus);
+            const char* dataStr;
+            char dataHex[3];
+            if (datapath.busfault) {
+                dataStr = "FLT";
+            } else {
+                snprintf(dataHex, sizeof dataHex, "%02X", datapath.databus);
+                dataStr = dataHex;
+            }
+            ImGui::Text("Data Bus: %s %c", dataStr,
+                        lines.readwrite ? 'R' : 'W');
 
             ImGui::Separator();
 
-            ImGui::TextUnformatted("Decode:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("RTS");
-            ImGui::TextUnformatted("adl:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("07");
-            ImGui::TextUnformatted("adh:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("00");
-            ImGui::TextUnformatted("adc:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("00");
-            ImGui::TextUnformatted("t:");
-            ImGui::SameLine();
-            ImGui::TextUnformatted("2");
+            const char* mnemonic;
+            char buf[DIS_DATAP_SIZE];
+            if (datapath.jammed) {
+                mnemonic = "JAMMED";
+            } else {
+                const auto wlen = dis_datapath(&state.snapshot, buf);
+                mnemonic = wlen < 0 ? dis_errstr(wlen) : buf;
+            }
+            ImGui::Text("Decode: %s", mnemonic);
+            ImGui::Text("adl: %02X", datapath.addrlow_latch);
+            ImGui::Text("adh: %02X", datapath.addrhigh_latch);
+            ImGui::Text("adc: %02X", datapath.addrcarry_latch);
+            ImGui::Text("t: %u", datapath.exec_cycle);
 
             ImGui::Separator();
 
-            static bool activeHi, activeLow = true;
-            ImGui::Checkbox("RDY", &activeHi);
+            ImGui::Text("RDY: %s", bool_to_linestate(lines.ready));
             ImGui::SameLine();
-            ImGui::Checkbox("SYNC", &activeHi);
+            ImGui::Text("SYNC: %s", bool_to_linestate(lines.sync));
             ImGui::SameLine();
-            ImGui::Checkbox("R/W", &activeHi);
+            ImGui::Text("R/W: %s", bool_to_linestate(lines.readwrite));
 
-            ImGui::Checkbox("IRQ", &activeLow);
+            ImGui::Text("IRQ: %s", bool_to_linestate(lines.irq));
             ImGui::SameLine();
-            ImGui::Checkbox("NMI ", &activeLow);
+            ImGui::Text("NMI:  %s", bool_to_linestate(lines.nmi));
             ImGui::SameLine();
-            ImGui::Checkbox("RES", &activeLow);
+            ImGui::Text("RES: %s", bool_to_linestate(lines.reset));
         }
 
+        const auto& prgMem = state.snapshot.mem;
         if (ImGui::CollapsingHeader("PRG @ PC")) {
-            static int selected = -1;
-            char buf[28];
-            std::uint16_t addr = 0x804f;
-            for (int i = 0; i < 7; ++i) {
-                std::snprintf(buf, sizeof buf, "%04X: FF FF FF *ISC $FFFF,X",
-                              addr);
-                if (ImGui::Selectable(buf, selected == i)) {
-                    selected = i;
+            static constexpr auto instCount = 7;
+            static auto selected = -1;
+            auto addr = state.snapshot.datapath.current_instruction;
+            dis_instruction inst{};
+            char disasm[DIS_INST_SIZE];
+            for (int i = 0; i < instCount; ++i) {
+                auto result = dis_parsemem_inst(prgMem.prglength,
+                                                prgMem.currprg,
+                                                inst.offset + inst.bv.size,
+                                                &inst);
+                if (result > 0) {
+                    result = dis_inst(addr, &inst, disasm);
+                    if (result > 0) {
+                        if (ImGui::Selectable(disasm, selected == i)) {
+                            selected = i;
+                        }
+                        addr += inst.bv.size;
+                        continue;
+                    }
                 }
-                addr += 3;
+                if (result < 0) {
+                    ImGui::TextUnformatted(dis_errstr(result));
+                }
+                break;
             }
         }
 
         if (ImGui::CollapsingHeader("Vectors")) {
-            ImGui::TextUnformatted("FFFA: 04 80     NMI $8004");
-            ImGui::TextUnformatted("FFFC: 00 80     RES $8000");
-            ImGui::TextUnformatted("FFFE: 04 80     IRQ $8004");
+            auto lo = prgMem.vectors[0], hi = prgMem.vectors[1];
+            ImGui::Text("%04X: %02X %02X     NMI $%04X", CPU_VECTOR_NMI, lo,
+                        hi, bytowr(lo, hi));
+
+            lo = prgMem.vectors[2];
+            hi = prgMem.vectors[3];
+            const auto& debugger = state.snapshot.debugger;
+            const char* indicator;
+            std::uint16_t resVector;
+            if (debugger.resvector_override >= 0) {
+                indicator = "!";
+                resVector = (std::uint16_t)debugger.resvector_override;
+            } else {
+                indicator = "";
+                resVector = bytowr(lo, hi);
+            }
+            ImGui::Text("%04X: %02X %02X     RES %s$%04X", CPU_VECTOR_RES, lo,
+                        hi, indicator, resVector);
+
+            lo = prgMem.vectors[4];
+            hi = prgMem.vectors[5];
+            ImGui::Text("%04X: %02X %02X     IRQ $%04X", CPU_VECTOR_IRQ, lo,
+                        hi, bytowr(lo, hi));
         }
     }
     ImGui::End();
@@ -323,19 +356,16 @@ void aldo::RenderFrame::renderCpu() const noexcept
 void aldo::RenderFrame::renderRam() const noexcept
 {
     if (ImGui::Begin("RAM")) {
-        static constexpr std::array<int, 0x800> testRam{};
         static constexpr auto pageSize = 0x100, pageDim = 0x10,
-                                cols = pageDim + 2;
-        static constexpr auto pageCount = testRam.size() / pageSize;
+                                cols = pageDim + 2, pageCount = 8;
         static constexpr auto tableConfig = ImGuiTableFlags_BordersOuter
                                             | ImGuiTableFlags_BordersV
                                             | ImGuiTableFlags_RowBg
                                             | ImGuiTableFlags_SizingFixedFit
                                             | ImGuiTableFlags_ScrollY;
-        static constexpr std::uint8_t stackPointer = 0xf4;
 
         const ImVec2 tableSize{
-            0, ImGui::GetTextLineHeightWithSpacing() * (pageDim + 1),
+            0, ImGui::GetTextLineHeightWithSpacing() * 2 * (pageDim + 1),
         };
         const auto spHighlightText
             = ImGui::ColorConvertU32ToFloat4(IM_COL32_BLACK);
@@ -350,6 +380,7 @@ void aldo::RenderFrame::renderRam() const noexcept
             ImGui::TableSetupColumn("ASCII");
             ImGui::TableHeadersRow();
 
+            const auto sp = state.snapshot.cpu.stack_pointer;
             std::uint16_t addr = 0;
             for (std::size_t page = 0; page < pageCount; ++page) {
                 for (std::size_t pageRow = 0; pageRow < pageDim; ++pageRow) {
@@ -363,12 +394,14 @@ void aldo::RenderFrame::renderRam() const noexcept
                     ImGui::TableNextRow();
                     ImGui::TableSetColumnIndex(0);
                     ImGui::Text("%04X", addr);
+                    const auto& lcl = std::locale::classic();
+                    char ascii[pageDim + 1]{};
                     for (std::size_t ramCol = 0; ramCol < pageDim; ++ramCol) {
                         const auto ramIdx = (page * pageSize)
                                             + (pageRow * pageDim) + ramCol;
-                        const auto val = testRam[ramIdx];
+                        const auto val = state.snapshot.mem.ram[ramIdx];
                         ImGui::TableSetColumnIndex((int)ramCol + 1);
-                        if (page == 1 && ramIdx % pageSize == stackPointer) {
+                        if (page == 1 && ramIdx % pageSize == sp) {
                             ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg,
                                                    IM_COL32(0xff, 0xfc, 0x53,
                                                             SDL_ALPHA_OPAQUE));
@@ -376,9 +409,12 @@ void aldo::RenderFrame::renderRam() const noexcept
                         } else {
                             ImGui::Text("%02X", val);
                         }
+                        ascii[ramCol] = std::isprint((char)val, lcl)
+                                        ? (char)val
+                                        : '.';
                     }
                     ImGui::TableSetColumnIndex(cols - 1);
-                    ImGui::TextUnformatted("................");
+                    ImGui::TextUnformatted(ascii);
                     addr += pageDim;
                 }
             }

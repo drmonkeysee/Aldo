2a0e1ec4164a14db32b62d2b5d5716502867388a
Brandon Stansbury <brandonrstansbury@gmail.com>
2022-12-18 19:21:55 -0800

    remove old swift targets

--- mac/AldoSwiftUI/Aldo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include "bytes.h"
-#include "cart.h"
-#include "cliargs.h"
-#include "cycleclock.h"
-#include "dis.h"

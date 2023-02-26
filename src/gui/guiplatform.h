f3ec6981c8fc68e1a11f849b4effc9ed44135128
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-02-26 14:45:26 -0800

    technically nodiscard cannot be applied to function pointer members

--- src/gui/guiplatform.h
+++ src/gui/guiplatform.h
@@ -12,15 +12,11 @@
 
 #include "bridgeopen.h"
 struct gui_platform {
-    br_ownresult
     char *(*appname)(void) br_nothrow;
-    br_ownresult
     char *(*orgname)(void) br_nothrow;
     bool (*is_hidpi)(void) br_nothrow;
     float (*render_scale_factor)(void *) br_nothrow;
-    br_ownresult
     char *(*open_file)(const char*, const char* const[]) br_nothrow;
-    br_ownresult
     char *(*save_file)(const char*, const char*) br_nothrow;
     void (*launch_studio)(void) br_nothrow;
     bool (*display_error)(const char *, const char *) br_nothrow;

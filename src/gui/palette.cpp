e21d59f66acc191d6a48f900cc8aa97a5a58d03d
Brandon Stansbury <brandonrstansbury@gmail.com>
2024-04-26 18:30:02 -0700

    add palette files

---
+++ src/gui/palette.cpp
@@ -0,0 +1,8 @@
+//
+//  palette.cpp
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/26/24.
+//
+
+#include "palette.hpp"
---
+++ src/gui/palette.hpp
@@ -0,0 +1,11 @@
+//
+//  palette.hpp
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/26/24.
+//
+
+#ifndef Aldo_gui_palette_hpp
+#define Aldo_gui_palette_hpp
+
+#endif

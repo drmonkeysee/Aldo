fe6aa7a779109f22a2eb048cf1929077098bc067
Brandon Stansbury <brandonrstansbury@gmail.com>
2024-04-27 16:35:22 -0700

    read pal file into memory

--- src/gui/palette.cpp
+++ src/gui/palette.cpp
@@ -7,7 +7,17 @@
 
 #include "palette.hpp"
 
-#include <stdexcept>
+#include "error.hpp"
+
+#include <fstream>
+#include <ios>
+
+namespace
+{
+
+constexpr aldo::palette::sz PalFileLength = aldo::palette::Size * 3;
+
+}
 
 //
 // Public Interface
@@ -18,8 +28,20 @@ aldo::palette::datav aldo::Palette::getColor(aldo::palette::sz idx) const
     return std::visit([idx](auto&& c) { return c[idx]; }, colors);
 }
 
-void aldo::Palette::load(const std::filesystem::path&)
+void aldo::Palette::load(const std::filesystem::path& filepath)
 {
-    // TODO: not implemented
-    throw std::runtime_error{"not implemented"};
+    std::ifstream f{filepath, std::ios::binary};
+    if (!f) throw aldo::AldoError{"Cannot open palette file", filepath};
+
+    std::array<std::ifstream::char_type, PalFileLength> buf{};
+    f.read(buf.data(), buf.size());
+    if (f.bad() || (f.fail() && !f.eof())) throw aldo::AldoError{
+        "Palette File Error", "Error reading palette file"
+    };
+    if (f.fail() && f.eof()) {
+        SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION,
+                    "Palette file too short;"
+                    " expected %zu bytes, read %zu bytes",
+                    buf.size(), f.gcount());
+    }
 }

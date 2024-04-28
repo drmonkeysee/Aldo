13b3246ed4416f0f9902a5ab56b457d2b8cf5556
Brandon Stansbury <brandonrstansbury@gmail.com>
2024-04-27 18:35:05 -0700

    pull color parse into helper

--- src/gui/palette.cpp
+++ src/gui/palette.cpp
@@ -15,13 +15,40 @@
 #include <iterator>
 #include <ranges>
 #include <tuple>
-#include <utility>
 
 namespace
 {
 
-constexpr aldo::palette::sz ByteFactor = 3;
-constexpr aldo::palette::sz PalFileLength = aldo::palette::Size * ByteFactor;
+constexpr aldo::palette::sz Channels = 3;
+constexpr aldo::palette::sz PalFileLength = aldo::palette::Size * Channels;
+
+using pal_buf = std::array<std::ifstream::char_type, PalFileLength>;
+
+auto parse_colors(const pal_buf& buf)
+{
+    auto newColors =
+        std::make_unique<aldo::palette::datav[]>(aldo::palette::Size);
+    aldo::palette::sz idx = 0;
+
+    // TODO: replace this with views::chunk in c++23
+    const auto end = buf.cend();
+    for (auto it = buf.cbegin();
+         it != end;
+         std::advance(it, Channels)) {
+        const auto t = std::views::take(std::ranges::subrange{it, end},
+                                        Channels);
+        // NOTE: t yields possibly signed chars; cast to byte, because int
+        // auto-promotion in IM_COL32 will sign-extend and mess up the shifts.
+        const auto [r, g, b] = std::tuple{
+            static_cast<aldo::et::byte>(t[0]),
+            static_cast<aldo::et::byte>(t[1]),
+            static_cast<aldo::et::byte>(t[2]),
+        };
+        newColors[idx++] = IM_COL32(r, g, b, SDL_ALPHA_OPAQUE);
+    }
+
+    return newColors;
+}
 
 }
 
@@ -39,7 +66,7 @@ void aldo::Palette::load(const std::filesystem::path& filepath)
     std::ifstream f{filepath, std::ios::binary};
     if (!f) throw aldo::AldoError{"Cannot open palette file", filepath};
 
-    std::array<std::ifstream::char_type, PalFileLength> buf{};
+    pal_buf buf{};
     f.read(buf.data(), buf.size());
     if (f.bad() || (f.fail() && !f.eof())) throw aldo::AldoError{
         "Palette File Error", "Error reading palette file"
@@ -50,24 +77,6 @@ void aldo::Palette::load(const std::filesystem::path& filepath)
                     " expected %zu bytes, read %zu bytes",
                     buf.size(), f.gcount());
     }
-    auto newColors =
-        std::make_unique<aldo::palette::datav[]>(aldo::palette::Size);
-    aldo::palette::sz idx = 0;
-    // TODO: replace this with views::chunk in c++23
-    const auto end = buf.cend();
-    for (auto it = buf.cbegin();
-         it != end;
-         std::advance(it, ByteFactor)) {
-        const auto t = std::views::take(std::ranges::subrange{it, end},
-                                        ByteFactor);
-        // NOTE: t yields possibly signed chars; cast to byte, because int
-        // auto-promotion in IM_COL32 will sign-extend and mess up the shifts.
-        const auto [r, g, b] = std::tuple{
-            static_cast<aldo::et::byte>(t[0]),
-            static_cast<aldo::et::byte>(t[1]),
-            static_cast<aldo::et::byte>(t[2]),
-        };
-        newColors[idx++] = IM_COL32(r, g, b, SDL_ALPHA_OPAQUE);
-    }
-    colors = std::move(newColors);
+
+    colors = parse_colors(buf);
 }

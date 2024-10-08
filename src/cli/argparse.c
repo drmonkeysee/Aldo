2eec78be9687fc8fa966ec248d4f95f5ed9acabb
Brandon Stansbury <brandonrstansbury@gmail.com>
2024-10-07 20:16:53 -0700

    reset errno before strtol

--- src/cli/argparse.c
+++ src/cli/argparse.c
@@ -75,6 +75,7 @@ static bool parse_flag(const char *arg, char shrt, bool exact, const char *lng)
 static bool convert_num(const char *restrict arg, int base, long *result)
 {
     char *end;
+    errno = 0;
     *result = strtol(arg, &end, base);
     if ((*result == LONG_MIN || *result == LONG_MAX) && errno == ERANGE) {
         perror("Number parse failed");

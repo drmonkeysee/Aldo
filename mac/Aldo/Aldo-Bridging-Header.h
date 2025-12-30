8c3f41cb85979cbb6c4e9ff6a015535232e8f797
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-12-30 14:32:44 -0800

    fix stdbool header issue for objective-c

--- mac/Aldo/Aldo-Bridging-Header.h
+++ mac/Aldo/Aldo-Bridging-Header.h
@@ -2,4 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// NOTE: Objective-C still needs the pre-C23 bool header to resolve type "bool"
+#include <stdbool.h>
+
 #include "guiplatform.h"

7b935abdc66f2a2ea8bb98f57291e1b591415d94
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-12-31 18:23:40 -0800

    update mac entry point to sdl3

--- mac/Aldo/main.m
+++ mac/Aldo/main.m
@@ -11,31 +11,25 @@
 #include "gui.h"
 #include "guiplatform.h"
 
-// NOTE: always include SDL with main in case SDL_main applies; this is also
-// why the app entrypoint is in Objective-C rather than Swift.
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_syswm.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 #include <stdlib.h>
 
-// NOTE: this function uses preprocessor macros and can't bridge to Swift
 static float render_scale_factor(void *sdlwindow)
 {
-    SDL_SysWMinfo winfo;
-    SDL_VERSION(&winfo.version);
-    if (SDL_GetWindowWMInfo(sdlwindow, &winfo)) {
-        if (winfo.subsystem == SDL_SYSWM_COCOA) {
-            return (float)winfo.info.cocoa.window.backingScaleFactor;
-        } else {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                         "Unexpected window subsystem found: %d",
-                         winfo.subsystem);
-        }
+    auto cocoa_win = (__bridge NSWindow *)SDL_GetPointerProperty(
+         SDL_GetWindowProperties(sdlwindow),
+         SDL_PROP_WINDOW_COCOA_WINDOW_POINTER,
+         nullptr
+    );
+    if (cocoa_win) {
+        return (float)cocoa_win.backingScaleFactor;
     } else {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
                      "Unable to retrieve window info: %s", SDL_GetError());
+        return 1.0f;
     }
-    return 1.0f;
 }
 
 static int run_app()

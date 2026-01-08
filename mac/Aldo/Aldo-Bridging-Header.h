f57c80dc7b70729037eea97a22e4de2b979b6c33
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-01-07 18:27:02 -0800

    refine bridging header comment

--- mac/Aldo/Aldo-Bridging-Header.h
+++ mac/Aldo/Aldo-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-// NOTE: Objective-C still needs the pre-C23 header to resolve bool type
+// NOTE: Swift-bridging still needs the pre-C23 header to resolve bool type
 #include <stdbool.h>
 
 #include "guiplatform.h"

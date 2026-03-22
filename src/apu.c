7a94eddce61f88e9728ed6c9f6cac54932398c49
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-03-22 12:55:27 -0700

    stub out apu powerup

--- src/apu.c
+++ src/apu.c
@@ -6,3 +6,19 @@
 //
 
 #include "apu.h"
+
+#include <assert.h>
+
+//
+// MARK: - Public Interface
+//
+
+void aldo_apu_powerup(struct aldo_rp2a03 *self)
+{
+    assert(self != nullptr);
+
+    aldo_cpu_powerup(&self->cpu);
+
+    // start the APU on a get cycle (in real hardware, startup state is random)
+    self->put = false;
+}
--- src/apu.h
+++ src/apu.h
@@ -13,7 +13,13 @@
 // The Ricoh RP2A03 Chip; includes the 6502 CPU, Audio Processing Unit (APU),
 // Direct Memory Access (DMA) units, and Joypad control.
 struct aldo_rp2a03 {
-    struct aldo_mos6502 cpu;
+    struct aldo_mos6502 cpu;    // 6502 CPU Core
+
+    bool put;                   // Whether the current cycle is a DMA get or put;
+                                // also used to count APU cycles per CPU cycles,
+                                // 2 CPU Cycles = 1 APU Cycle.
 };
+
+void aldo_apu_powerup(struct aldo_rp2a03 *self);
 
 #endif

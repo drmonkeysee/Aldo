2240989d2d3ffd5eef7d151ffde609cfce375d03
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-04-05 17:29:52 -0700

    add sprite last row test

--- test/ppurender.c
+++ test/ppurender.c
@@ -1202,7 +1202,7 @@ static void render_not_set_vertical_coords(void *ctx)
 // MARK: - Sprite Fetches
 //
 
-static void sprite_fetch_sequence(void *ctx)
+static void sprite_fetch_first_row(void *ctx)
 {
     auto ppu = ppt_get_ppu(ctx);
     NameTables[0][3] = 0x99;
@@ -1350,6 +1350,159 @@ static void sprite_fetch_sequence(void *ctx)
     ct_assertequal(43u, ppu->pxpl.spu[0].x);
     ct_assertequal(0x33u, ppu->pxpl.spu[0].fg[0]);
     ct_assertequal(0x44u, ppu->pxpl.spu[0].fg[1]);
+    ct_assertfalse(ppu->signal.ale);
+    ct_assertfalse(ppu->signal.rd);
+}
+
+static void sprite_fetch_last_row(void *ctx)
+{
+    auto ppu = ppt_get_ppu(ctx);
+    NameTables[0][3] = 0x99;
+    NameTables[0][5] = 0x77;
+    PatternTables[0][7] = 0xaa;
+    PatternTables[0][15] = 0xbb;
+    ppu->v = 0x5;
+    ppu->t = 0x7b3;
+    ppu->line = 7;
+    ppu->dot = 257;
+    ppu->pxpl.spuidx = 72;
+    ppu->spr.soam[1] = 0;       // y-coord
+    ppu->spr.soam[1] = 0x3;     // tile id
+    ppu->spr.soam[2] = 0x2;     // fv:0, hv:0, pri:fg, pal:2
+    ppu->spr.soam[3] = 43;      // x-coord
+    ppu->spr.soaddr = 0x4;      // 1 active sprite in secondary OAM
+    ppu->oamaddr = 0xab;
+
+    // Unused NT Fetch
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(258, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x2005u, ppu->vaddrbus);
+    ct_assertequal(0u, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(0u, ppu->pxpl.spu[0].a);
+    ct_assertequal(0u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_asserttrue(ppu->signal.ale);
+    ct_asserttrue(ppu->signal.rd);
+
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(259, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x2005u, ppu->vaddrbus);
+    ct_assertequal(0x77u, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(0u, ppu->pxpl.spu[0].a);
+    ct_assertequal(0u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_assertfalse(ppu->signal.ale);
+    ct_assertfalse(ppu->signal.rd);
+
+    // Ignored NT Fetch + Latch Sprite Attribute
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(260, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x2413u, ppu->vaddrbus);
+    ct_assertequal(0x77u, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(0u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_asserttrue(ppu->signal.ale);
+    ct_asserttrue(ppu->signal.rd);
+
+    // Latch Sprite X-Coord
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(261, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x2413u, ppu->vaddrbus);
+    ct_assertequal(0x99u, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(43u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_assertfalse(ppu->signal.ale);
+    ct_assertfalse(ppu->signal.rd);
+
+    // Sprite Fetch Low
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(262, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x37u, ppu->vaddrbus);
+    ct_assertequal(0x99u, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(43u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_asserttrue(ppu->signal.ale);
+    ct_asserttrue(ppu->signal.rd);
+
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(263, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x37u, ppu->vaddrbus);
+    ct_assertequal(0xaau, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(43u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0xaau, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_assertfalse(ppu->signal.ale);
+    ct_assertfalse(ppu->signal.rd);
+
+    // Sprite Fetch High
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(264, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x3fu, ppu->vaddrbus);
+    ct_assertequal(0xaau, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(0u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(43u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0xaau, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0u, ppu->pxpl.spu[0].fg[1]);
+    ct_asserttrue(ppu->signal.ale);
+    ct_asserttrue(ppu->signal.rd);
+
+    aldo_ppu_cycle(ppu);
+
+    ct_assertequal(265, ppu->dot);
+    ct_assertequal(0x413u, ppu->v);
+    ct_assertequal(0x3fu, ppu->vaddrbus);
+    ct_assertequal(0xbbu, ppu->vdatabus);
+    ct_assertequal(0u, ppu->oamaddr);
+    ct_assertequal(4u, ppu->spr.soaddr);
+    ct_assertequal(1u, ppu->pxpl.spuidx);
+    ct_assertequal(2u, ppu->pxpl.spu[0].a);
+    ct_assertequal(43u, ppu->pxpl.spu[0].x);
+    ct_assertequal(0xaau, ppu->pxpl.spu[0].fg[0]);
+    ct_assertequal(0xbbu, ppu->pxpl.spu[0].fg[1]);
     ct_assertfalse(ppu->signal.ale);
     ct_assertfalse(ppu->signal.rd);
 }
@@ -3748,7 +3901,8 @@ struct ct_testsuite ppu_render_tests()
         ct_maketest(prerender_set_vertical_coords),
         ct_maketest(render_not_set_vertical_coords),
 
-        ct_maketest(sprite_fetch_sequence),
+        ct_maketest(sprite_fetch_first_row),
+        ct_maketest(sprite_fetch_last_row),
 
         ct_maketest(course_x_wraparound),
         ct_maketest(fine_y_wraparound),

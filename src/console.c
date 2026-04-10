f3e8bcf82076fbf87d7874ecfe4c9d03b19c7187
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-04-10 16:30:47 -0700

    add base console module

---
+++ src/console.c
@@ -0,0 +1,8 @@
+//
+//  console.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/10/26.
+//
+
+#include "console.h"
---
+++ src/console.h
@@ -0,0 +1,11 @@
+//
+//  console.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/10/26.
+//
+
+#ifndef Aldo_console_h
+#define Aldo_console_h
+
+#endif

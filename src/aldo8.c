dd08a139fe850516e9f119c72a4145d6bdf9a401
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-04-10 16:04:19 -0700

    add aldo8 files

---
+++ src/aldo8.c
@@ -0,0 +1,8 @@
+//
+//  aldo8.c
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/10/26.
+//
+
+#include "aldo8.h"
---
+++ src/aldo8.h
@@ -0,0 +1,11 @@
+//
+//  aldo8.h
+//  Aldo
+//
+//  Created by Brandon Stansbury on 4/10/26.
+//
+
+#ifndef Aldo_aldo8_h
+#define Aldo_aldo8_h
+
+#endif

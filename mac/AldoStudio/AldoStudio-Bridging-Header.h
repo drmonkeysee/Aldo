6ca9194e142681845f3e86b0a0050213a15d7624
Brandon Stansbury <brandonrstansbury@gmail.com>
2026-04-24 20:59:04 -0700

    remove AldoStudio project

--- mac/AldoStudio/AldoStudio-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include "bytes.h"
-#include "cart.h"
-#include "ctrlsignal.h"
-#include "dis.h"
